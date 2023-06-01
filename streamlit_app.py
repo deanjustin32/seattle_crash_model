968751eadf42492e5ad129e4f0a8a08260681794
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-06-01 16:59:45 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,27 @@ import streamlit as st
 
 def tab1():
     st.title("Home")
-    st.components.v1.html(open("base.HTML", 'r').read(), width=1000, height=10000)
+    st.components.v1.html(open("base.html", 'r').read(), width=1000, height=1000)
 
 def tab2():
     st.title("Prediction Tool")
-    st.components.v1.html(open("tool.HTML", 'r').read(), width=1000, height=10000)
+    st.components.v1.html(open("tool.html", 'r').read(), width=1000, height=1000)
 
 def tab3():
     st.title("Map")
-    st.components.v1.html(open("map.html", 'r').read(), width=1000, height=10000)
+    st.components.v1.html(open("map.html", 'r').read(), width=1000, height=1000)
 
 def tab4():
     st.title("Graphs")
-    st.components.v1.html(open("graphs.HTML", 'r').read(),width=1000, height=10000)
+    st.components.v1.html(open("graphs.html", 'r').read(), width=1000, height=1000)
 
 def tab5():
     st.title("The Dream Team")
-    st.components.v1.html(open("team.html", 'r').read(),width=1000, height=10000)
+    st.components.v1.html(open("team.html", 'r').read(), width=1000, height=1000)
 
 # Main app
 def main():
-    st.sidebar.title("Directory")
+    st.title("Directory")
     
     tabs = {
         "Home": tab1,
@@ -32,22 +32,17 @@ def main():
         "The Dream Team": tab5
     }
     
-    active_tab = st.sidebar.radio(" ", list(tabs.keys()))
-    # tabs[active_tab]()  # Call the selected tab function
+    active_tab = st.sidebar.radio("Select a tab", list(tabs.keys()))
 
-    st.sidebar.title("Directory")
-    active_tab = st.sidebar.radio(" ", list(tabs.keys()), index=list(tabs.keys()).index(active_tab))
+    # Render the sidebar
+    with st.sidebar:
+        st.title("Directory")
+        active_tab = st.radio(" ", list(tabs.keys()), index=list(tabs.keys()).index(active_tab))
 
-    col1, col2 = st.columns([1, 4])
-
-    # Place the HTML content in the second column
-    with col2:
+    # Render the main content
+    with st.container():
         st.title(active_tab)
         tabs[active_tab]()  # Call the selected tab function
-
-    # Add an empty column to align the HTML window to the left
-    with col1:
-        st.empty()
 
 if __name__ == "__main__":
     main()
