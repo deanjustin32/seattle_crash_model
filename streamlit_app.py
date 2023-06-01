9db7f3bda88d615e7562a6b868f30a099b93c1dc
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-05-31 20:57:34 -0700

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-from streamlit import components
 
 # Read the HTML file content
 with open("./static/base.HTML", "r") as file:
     html_content = file.read()
 
- # Set the display size
- height = 5800  # Adjust the height as needed
- width = 51000  # Adjust the width as needed  
-
 # Render the HTML content using Streamlit
-st.components.v1.html(html_content)
+st.components.v1.html(html_content, height=800, scrolling=True)
