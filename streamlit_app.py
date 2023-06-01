55795cc3a9f40fbcf20259d67a1dfff200456703
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-05-31 21:16:43 -0700

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,6 @@ import streamlit as st
 with open("./static/base.HTML", "r") as file:
     html_content = file.read()
 
-# Center the content using Streamlit's layout options
-st.markdown(
-    f"""
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-        {html_content}
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 # Render the HTML content using Streamlit
-# st.components.v1.html(html_content, height=800, width=1500, scrolling=True)
+st.components.v1.html(html_content, height=800, width=900, scrolling=True)
