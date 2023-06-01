b7001b86a274caa0218fd92a1645ffbf17d367bc
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-05-31 21:01:32 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ with open("./static/base.HTML", "r") as file:
     html_content = file.read()
 
 # Render the HTML content using Streamlit
-st.components.v1.html(html_content, height=800, scrolling=True)
+st.components.v1.html(html_content, height=800, width=600, scrolling=True)
