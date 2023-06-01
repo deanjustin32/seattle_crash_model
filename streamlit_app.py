212c666859f13debcf3f2a3f19b2edcdbfb885fa
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-06-01 14:59:09 -0700

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,39 @@
 import streamlit as st
 
-# Read the HTML file content
-with open("./static/base.HTML", "r") as file:
-    html_content = file.read()
+def tab1():
+    st.title("Tab 1")
+    st.components.v1.html(open("./static/base.html", 'r').read())
 
+def tab2():
+    st.title("Tab 2")
+    st.components.v1.html(open("./static/tool.html", 'r').read())
 
-# Render the HTML content using Streamlit
-st.components.v1.html(html_content, height=800, width=900, scrolling=True)
+def tab3():
+    st.title("Tab 3")
+    st.components.v1.html(open("./static/map.html", 'r').read())
+
+def tab4():
+    st.title("Tab 4")
+    st.components.v1.html(open("./static/graphs.html", 'r').read())
+
+def tab5():
+    st.title("Tab 5")
+    st.components.v1.html(open("team.html", 'r').read())
+
+# Main app
+def main():
+    st.sidebar.title("HTML Tabs")
+    
+    tabs = {
+        "Home": tab1,
+        "Prediction Tool": tab2,
+        "Map": tab3,
+        "Graphs": tab4,
+        "Team": tab5
+    }
+    
+    active_tab = st.sidebar.radio("Select Tab", list(tabs.keys()))
+    tabs[active_tab]()  # Call the selected tab function
+
+if __name__ == "__main__":
+    main()
