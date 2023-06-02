0422e84fd45aab78239ce667b5abb8d33008f6b5
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-06-01 22:59:37 -0700

    message

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,50 @@ def tab2():
 
     st.write("Accident Conditions:")
 
-    accident_location = ["Block", "Intersection"]
+    block_or_intersection = ["Block", "Intersection"]
 
+    collision_type = ['Parked Car', 'Angles', 'Rear Ended', 'Sideswipe', 'Left Turn', 'Pedestrian', 'Cycles', 'Right Turn', 'Head On', 'Other']
 
-    feature1 = st.selectbox("Where did this accident take place?", accident_location)
-    feature2 = st.text_input("Feature 2")
-    feature3 = st.text_input("Feature 2")
+    collision_place = ['Mid-Block (not related to intersection)','At Intersection (intersection related)', 'Mid-Block (but intersection related)'
+                       , 'Driveway Junction', 'At Intersection (but not related to intersection)','At Intersection (but not related to intersection)',
+                        'Ramp Junction', 'Unknown' ]
+    
+    light_conditions = ['Daylight', 'Dark', 'Dusk', 'Dawn', 'Unknown', 'Other']
+
+    road_conditions = ['Wet', 'Dry', 'Icy', 'Unknown']
+
+    crash_conditions = ['MOTOR VEHICLE STRUCK MOTOR VEHICLE', 'MOTOR VEHICLE RAN OFF ROAD', 'MOTOR VEHICLE STRUCK PEDESTRIAN',
+                        'NOT ENOUGH INFORMATION / NOT APPLICABLE', 'MOTOR VEHICLE STRUCK OBJECT IN ROAD',
+                        'MOTOR VEHICLE STRUCK PEDALCYCLIST', 'PEDALCYCLIST STRUCK MOTOR VEHICLE', 'MOTOR VEHICLE OVERTURNED IN ROAD',
+                        'DRIVERLESS VEHICLE STRUCK MOTOR VEHICLE', 'DRIVERLESS VEHICLE RAN OFF ROAD', 'MOTOR VEHICLE STRUCK TRAIN',
+                        'PEDALCYCLIST STRUCK PEDESTRIAN', 'PEDALCYCLIST OVERTURNED IN ROAD', 'PEDALCYCLIST STRUCK OBJECT IN ROAD',
+                        'DRIVERLESS VEHICLE STRUCK PEDESTRIAN', 'PEDALCYCLIST STRUCK PEDALCYCLIST REAR END',
+                        'PEDALCYCLIST STRUCK PEDALCYCLIST FRONT END AT ANGLE', 'PEDALCYCLIST RAN OFF ROAD - HIT FIXED OBJECT',
+                        'DRIVERLESS VEHICLE STRUCK OBJECT IN ROADWAY', 'MOTORIZED SCOOTER COLLISION: COLLISION INVOLVING A MOTORIZED SCOOTER']
+    
+    yes_no = ['Y', 'N']
+
+    weather = ['Clear', 'Raining', 'Overcast', 'Unknown', 'Snowing', 'Other', 'Fog/Smog/Smoke', 'Sleet/Hail/Freezing Rain',
+               'Blowing Sand/Dirt', 'Severe Crosswind', 'Partly Cloudy', 'Blowing Snow']
+
+    feature1 = st.selectbox("Block or Intersection?", block_or_intersection)
+    feature2 = st.selectbox("What was the collision type?", collision_type)
+    feature3 = st.selectbox("Where did the collision take place?", collision_place)
+    feature4 = st.selectbox("What were the light conditions?", light_conditions)
+    feature5 = st.number_input("How many pedestrians were involved?", value=0, step=1)
+    feature6 = st.number_input("How many cyclists were involved?", value=0, step=1)
+    feature7 = st.number_input("How many people were involved?", value=0, step=1)
+    feature8 = st.selectbox("What were the road conditions?", road_conditions)
+    feature9 = st.selectbox("Which of these options fits your condition the best?", crash_conditions)
+    feature10 = st.selectbox("Was anyone under the influence at the time of the accident?", yes_no)
+    feature11 = st.number_input("How many vehicles were involved?", value=0, step=1)
+    feature12 = st.selectbox("What was the weather at the time of the accident?", weather)
+    feature13 = st.selectbox("Was a parked car hit during this accident?", yes_no)
+
 
     if st.button("Predict"):
         # Prepare the input features for prediction
-        input_features = [feature1, feature2]  # Add more features as needed
+        input_features = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11, feature12, feature13]
         
         # Perform prediction using the loaded model
         prediction = model.predict([input_features])[0]
