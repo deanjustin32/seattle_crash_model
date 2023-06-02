7a69c4f9c644b1b258c2f53a2557e5704f03f879
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-06-01 17:01:46 -0700

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,27 @@ import streamlit as st
 
 def tab1():
     st.title("Home")
-    st.components.v1.html(open("base.html", 'r').read(), width=1000, height=1000)
+    st.components.v1.html(open("base.HTML", 'r').read(), width=1000, height=10000)
 
 def tab2():
     st.title("Prediction Tool")
-    st.components.v1.html(open("tool.html", 'r').read(), width=1000, height=1000)
+    st.components.v1.html(open("tool.HTML", 'r').read(), width=1000, height=10000)
 
 def tab3():
     st.title("Map")
-    st.components.v1.html(open("map.html", 'r').read(), width=1000, height=1000)
+    st.components.v1.html(open("map.html", 'r').read(), width=1000, height=10000)
 
 def tab4():
     st.title("Graphs")
-    st.components.v1.html(open("graphs.html", 'r').read(), width=1000, height=1000)
+    st.components.v1.html(open("graphs.HTML", 'r').read(),width=1300, height=10000)
 
 def tab5():
     st.title("The Dream Team")
-    st.components.v1.html(open("team.html", 'r').read(), width=1000, height=1000)
+    st.components.v1.html(open("team.html", 'r').read(),width=1000, height=10000)
 
 # Main app
 def main():
-    st.title("Directory")
+    st.sidebar.title("Directory")
     
     tabs = {
         "Home": tab1,
@@ -32,17 +32,8 @@ def main():
         "The Dream Team": tab5
     }
     
-    active_tab = st.sidebar.radio("Select a tab", list(tabs.keys()))
-
-    # Render the sidebar
-    with st.sidebar:
-        st.title("Directory")
-        active_tab = st.radio(" ", list(tabs.keys()), index=list(tabs.keys()).index(active_tab))
-
-    # Render the main content
-    with st.container():
-        st.title(active_tab)
-        tabs[active_tab]()  # Call the selected tab function
+    active_tab = st.sidebar.radio(" ", list(tabs.keys()))
+    tabs[active_tab]()  # Call the selected tab function
 
 if __name__ == "__main__":
     main()
